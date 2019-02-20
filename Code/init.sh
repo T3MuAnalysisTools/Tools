6d7e0ea4b5413e2317a3b2321a1396139a2bd888
Vladimir <vladimir.cherepanov@cern.ch>
2019-02-20 12:57:04 +0100

    remove junk

--- Code/init.sh
+++
@@ -1,5 +0,0 @@
-#!bin/zsh
-source /cvmfs/sft.cern.ch/lcg/external/gcc/4.9.1/x86_64-slc6-gcc48-opt/setup.sh
-cd /cvmfs/sft.cern.ch/lcg/external/gcc/4.9.1/x86_64-slc6-gcc48-opt/
-source setup.sh
-cd -
