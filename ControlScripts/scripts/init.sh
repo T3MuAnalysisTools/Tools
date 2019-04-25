027de469e77f89e58a426e006866578c19b71721
Bhargav Madhusudan Joshi <bhargav.madhusudan.joshi@cern.ch>
2019-04-25 14:41:09 +0200

    Updating new scripts

---
+++ ControlScripts/scripts/init.sh
@@ -0,0 +1,6 @@
+cd  /afs/cern.ch/work/b/bjoshi/Analysis/workdirMCStudyApr_18_2019/Code/CommonUtils/CMSSW_9_3_8/src
+cmsenv
+cd -
+source compile --joshi
+voms-proxy-init --voms cms
+cp /tmp/x509up_u92327 /afs/cern.ch/work/b/bjoshi/private/Tau23Mu/test/T3MuAnalysisTools/Tools/ControlScripts/proxy/x509up_u92327
