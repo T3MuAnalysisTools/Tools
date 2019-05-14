db162631d4de68ac8ce49369ec51cee016b9e495
Bhargav Madhusudan Joshi <bhargav.madhusudan.joshi@cern.ch>
2019-05-14 13:52:52 +0200

    Re-adding PDG_Var

---
+++ Code/PDG_Var.cxx
@@ -0,0 +1,15 @@
+#include "PDG_Var.h"
+
+double PDG_Var::Z_mass(){return 91.1876;}//GeV                                                                                                   
+double PDG_Var::Z_width(){return 2.4952;}//GeV   
+double PDG_Var::Tau_mass(){return 1.777;}//GeV   
+double PDG_Var::W_mass(){return 80.399;}//GeV 
+double PDG_Var::W_width(){return 2.085;}//GeV 
+double PDG_Var::Top_mass(){return 172.9;}//GeV
+double PDG_Var::Top_width(){return 2.0;}//GeV
+double PDG_Var::Phi_mass(){return 1.019;}//GeV
+double PDG_Var::Phi_width(){return 4.26/1000.;}//GeV
+double PDG_Var::Rho_mass(){return 0.770;}//GeV
+double PDG_Var::Rho_width(){return 0.140;}//GeV
+double PDG_Var::Omega_mass(){return 0.778;}//GeV
+double PDG_Var::Omega_width(){return 8.5/1000;}//GeV
