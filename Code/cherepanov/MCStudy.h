ca1684d5015b2dde3a7a918c738bfc078ea28f1c
Vladimir <vladimir.cherepanov@cern.ch>
2019-06-26 11:50:07 +0200

    cherepanov

--- Code/cherepanov/MCStudy.h
+++ Code/cherepanov/MCStudy.h
@@ -14,14 +14,7 @@ class MCStudy : public Selection {
   virtual void  Configure();
   virtual void  Finish();
 
-  /////////////////////////////////////////////////////////
-  // This is a cut enumerator, for other event cuts please
-  // fill the enumerator with put a new enumarator with an 
-  // understandanle name, for exmaple  enum cuts {TriggerOk=0,
-  // PrimeVts, EventCut1, EventCut2, ..., NCuts};  
-  // Do not remove/rename  the last enumerator   NCuts;
-
-  enum cuts {isRecoCut=0, L1SeedOk,HLTOk,PrimeVtx, NCuts}; 
+  enum cuts {TriggerOk=0,SignalCandidate, Mu1PtCut, Mu2PtCut, Mu3PtCut, MuonID, PhiVeto, OmegaVeto, TriggerMatch, ThreeMuMass,NCuts}; 
 
 
  protected:
@@ -29,41 +22,64 @@ class MCStudy : public Selection {
   virtual void Store_ExtraDist();
 
  private:
+  double tauMinMass_, tauMaxMass_;
+  double tauMinSideBand_,tauMaxSideBand_;
+
+
   // Selection Variables
   // Initializhere your analysis histograms
-  std::vector<TH1D> NVtx;
-  std::vector<TH1D> MuonsPt;
-  std::vector<TH1D> MuonsEta;
-  std::vector<TH1D> MuonsPhi;
-  std::vector<TH1D> PhiMass;
-  std::vector<TH1D> TripleMass;
-  std::vector<TH2D> PhiMassVsDsMass;
-  std::vector<TH1D>  MuonsPtRatio;
-  std::vector<TH1D> Category;
 
-  std::vector<TH1D> FirstMuonsPt;
-  std::vector<TH1D> SecondMuonsPt;
-  std::vector<TH1D> ThirdMuonsPt;
+  std::vector<TH1D> Muon1Pt;
+  std::vector<TH1D> Muon2Pt;
+  std::vector<TH1D> Muon3Pt;
 
-  std::vector<TH1D> FirstMuonsEta;
-  std::vector<TH1D> SecondMuonsEta;
-  std::vector<TH1D> ThirdMuonsEta;
-
-  std::vector<TH1D> Muon1_Match_dR;
-  std::vector<TH1D> Muon2_Match_dR;
-  std::vector<TH1D> Muon3_Match_dR;
-
-  std::vector<TH1D> Preselection;
-  std::vector<TH1D> Muon1_Pass_Standard_Selectors;
-
-  std::vector<TH1D> Muon1_ID;
-  std::vector<TH1D> Muon2_ID;
-  std::vector<TH1D> Muon3_ID;
+  std::vector<TH1D> Muon1Eta;
+  std::vector<TH1D> Muon2Eta;
+  std::vector<TH1D> Muon3Eta;
 
 
-  std::vector<TH1D> Muon1_isGlob;
-  std::vector<TH1D> Muon2_isGlob;
-  std::vector<TH1D> Muon3_isGlob;
+
+  std::vector<TH1D> TauMassResolution;
+  std::vector<TH1D> TauMassResolutionRefit;
+
+  std::vector<TH1D> Muon1isGlob;
+  std::vector<TH1D> Muon2isGlob;
+  std::vector<TH1D> Muon3isGlob;
+
+  std::vector<TH1D> Muon1isStand;
+  std::vector<TH1D> Muon2isStand;
+  std::vector<TH1D> Muon3isStand;
+
+  std::vector<TH1D> Muon1isTrack;
+  std::vector<TH1D> Muon2isTrack;
+  std::vector<TH1D> Muon3isTrack;
+
+
+  std::vector<TH1D> Muon1PtResolution;
+  std::vector<TH1D> Muon2PtResolution;
+  std::vector<TH1D> Muon3PtResolution;
+
+  std::vector<TH1D> Muon1EtaResolution;
+  std::vector<TH1D> Muon2EtaResolution;
+  std::vector<TH1D> Muon3EtaResolution;
+
+  std::vector<TH1D> Muon1DRToTruth;
+  std::vector<TH1D> Muon2DRToTruth;
+  std::vector<TH1D> Muon3DRToTruth;
+
+  std::vector<TH2D> TauMassResolutionVsEta;
+  std::vector<TH2D> TauMassResolutionVsPt;
+
+  std::vector<TH2D> TauMassResolutionVsMu1Eta;
+  std::vector<TH2D> TauMassResolutionVsMu1Pt;
+
+  std::vector<TH2D> TauMassResolutionVsMu2Eta;
+  std::vector<TH2D> TauMassResolutionVsMu2Pt;
+
+  std::vector<TH2D> TauMassResolutionVsMu3Eta;
+  std::vector<TH2D> TauMassResolutionVsMu3Pt;
+
+
 
 
 
