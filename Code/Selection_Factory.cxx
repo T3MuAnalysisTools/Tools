33a6d81e6202b763201a21ff78195c4dc5750f2f
Bhargav Madhusudan Joshi <bhargav.madhusudan.joshi@cern.ch>
2019-07-10 18:39:00 +0200

    NewTMVAVars class added

--- Code/Selection_Factory.cxx
+++ Code/Selection_Factory.cxx
@@ -18,6 +18,7 @@
 #include "joshi/ThreeMu.h"
 #include "joshi/TMVASignal.h"
 #include "joshi/AnalysisWithTMVA.h"
+#include "joshi/NewTMVAVars.h"
 #include "joshi/MCEfficiency.h"
 #include "joshi/twoGlbOneLooseMuIdCuts.h"
 #include "joshi/looseMuIdCuts.h"
@@ -102,6 +103,8 @@ Selection_Base* Selection_Factory::Factory(TString Analysis, TString UncertType,
   else if(Analysis.Contains("triplemuisolation"))s=new TripleMuIsolation(Analysis,UncertType); // calorimeter isolation variables 
   else if(Analysis.Contains("mcefficiency"))s=new MCEfficiency(Analysis,UncertType);
   else if(Analysis.Contains("analysiswithtmva"))s=new AnalysisWithTMVA(Analysis,UncertType);
+  else if(Analysis.Contains("newtmvavars"))s=new NewTMVAVars(Analysis,UncertType);
+
   else if(Analysis.Contains("glbmuidcuts"))s=new glbMuIdCuts(Analysis,UncertType);
   else if(Analysis.Contains("loosemuidcuts"))s=new looseMuIdCuts(Analysis,UncertType);
   else if(Analysis.Contains("softmuidcuts"))s=new softMuIdCuts(Analysis,UncertType);
