e1bb1e245794323124194407e53f131466b2cc95
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2020-10-02 15:19:54 +0200

    change in combineSignalData for more convenient options

--- Code/macros/combineSignalData.py
+++ Code/macros/combineSignalData.py
@@ -8,39 +8,39 @@ import os
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f1", "--first-file",help="Input root file with data tree", type = str, action="store", default = 'FillMVATree_2017_merged.root')
-    parser.add_argument("-f2", "--second-file",help="Input root file with  MC (DsTau) tree", type = str, action="store", default = 'FillMVATree_DsToTau_merged.root')
-    parser.add_argument("-f3", "--third-file",help="Input root file with  MC (BuTau) tree", type = str, action="store", default = 'FillMVATree_BuToTau_merged.root')
-    parser.add_argument("-f4", "--fourth-file",help="Input root file with  MC (BdTau) tree", type = str, action="store", default = 'FillMVATree_BdToTau_merged.root')
-    parser.add_argument("-o",  "--output-file", help="Output root file", type = str, action="store", default='FillMVATreeInput_combined.root')
+    parser.add_argument("-data", "--data",help="Input root file with data tree", type = str, action="store", default = 'FillMVATree_2017_merged.root')
+    parser.add_argument("-ds",   "--ds",help="Input root file with  MC (DsTau) tree", type = str, action="store", default = 'FillMVATree_DsToTau_merged.root')
+    parser.add_argument("-bp",   "--bp",help="Input root file with  MC (BpTau) tree", type = str, action="store", default = 'FillMVATree_BpToTau_merged.root')
+    parser.add_argument("-b0",   "--b0",help="Input root file with  MC (B0Tau) tree", type = str, action="store", default = 'FillMVATree_B0ToTau_merged.root')
+    parser.add_argument("-o",    "--output-file", help="Output root file", type = str, action="store", default='FillMVATreeInput_combined.root')
     args = parser.parse_args()
     cwd = os.getcwd()
 
-    input_file_1 = ROOT.TFile(args.first_file, "READ")
-    tree1 = input_file_1.Get("tree")
+    input_file_data = ROOT.TFile(args.data, "READ")
+    tree_data = input_file_data.Get("tree")
 
-    input_file_2 = ROOT.TFile(args.second_file, "READ")
-    tree2 = input_file_2.Get("tree")
+    input_file_ds = ROOT.TFile(args.ds, "READ")
+    tree_ds = input_file_ds.Get("tree")
     
-    input_file_3 = ROOT.TFile(args.third_file, "READ")
-    tree3 = input_file_3.Get("tree")
+    input_file_bp = ROOT.TFile(args.bp, "READ")
+    tree_bp = input_file_bp.Get("tree")
 
-    input_file_4 = ROOT.TFile(args.fourth_file, "READ")
-    tree4 = input_file_4.Get("tree")   
+    input_file_b0 = ROOT.TFile(args.b0, "READ")
+    tree_b0 = input_file_b0.Get("tree")   
     
     output_file = ROOT.TFile(args.output_file, "RECREATE")
 
-    new_tree1=tree1.CloneTree()
-    new_tree1.SetName("TreeB")
+    new_tree_data=tree_data.CloneTree()
+    new_tree_data.SetName("TreeB")
 
-    new_tree2=tree2.CloneTree()
-    new_tree2.SetName("TreeS_Ds")
+    new_tree_ds=tree_ds.CloneTree()
+    new_tree_ds.SetName("TreeS_Ds")
     output_file.Write()
 
-    new_tree3=tree3.CloneTree()
-    new_tree3.SetName("TreeS_Bu")
+    new_tree_bp=tree_bp.CloneTree()
+    new_tree_bp.SetName("TreeS_Bu")
     output_file.Write()
     
-    new_tree4=tree4.CloneTree()
-    new_tree4.SetName("TreeS_Bd")
+    new_tree_b0=tree_b0.CloneTree()
+    new_tree_b0.SetName("TreeS_Bd")
     output_file.Write()
