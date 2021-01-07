7be8b7543c7e82d12c73fe5c2d060231c7295a80
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2021-01-07 18:20:10 +0100

    small fix

--- Code/CommonUtils/IterativeTrain/TMVAClassification.py
+++ Code/CommonUtils/IterativeTrain/TMVAClassification.py
@@ -189,7 +189,6 @@ if __name__ == "__main__":
 
     # Run TMVA with given configs
     from ROOT import TMVA
-    print len(configuration)
     doTrain(configuration, selection, mlist,infname)
 
     
