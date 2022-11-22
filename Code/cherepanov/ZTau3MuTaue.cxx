8ffcc14e2f9337f9e64b121918b9cfe1d5e9ef86
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2022-11-22 21:57:31 +0100

    add ZTauTau Selectors

--- Code/cherepanov/ZTau3MuTaue.cxx
+++ Code/cherepanov/ZTau3MuTaue.cxx
@@ -1,4 +1,4 @@
-#include "ZTau3MuTauh.h"
+#include "ZTau3MuTaue.h"
 #include "TLorentzVector.h"
 #include <cstdlib>
 #include "HistoConfig.h"
@@ -6,13 +6,13 @@
 #include <iostream>
 //#include "Logger.h"
 
-ZTau3MuTauh::ZTau3MuTauh(TString Name_, TString id_):
+ZTau3MuTaue::ZTau3MuTaue(TString Name_, TString id_):
   Selection(Name_,id_)
 {
   // This is a class constructor;
 }
 
-ZTau3MuTauh::~ZTau3MuTauh(){
+ZTau3MuTaue::~ZTau3MuTaue(){
   for(unsigned int j=0; j<Npassed.size(); j++){
 	 Logger(Logger::Info) << "Selection Summary before: "
 	 << Npassed.at(j).GetBinContent(1)     << " +/- " << Npassed.at(j).GetBinError(1)     << " after: "
@@ -21,7 +21,7 @@ ZTau3MuTauh::~ZTau3MuTauh(){
   Logger(Logger::Info) << "complete." << std::endl;
 }
 
-void  ZTau3MuTauh::Configure(){
+void  ZTau3MuTaue::Configure(){
 
   for(int i=0; i<NCuts;i++){
     cut.push_back(0);
@@ -30,12 +30,9 @@ void  ZTau3MuTauh::Configure(){
     if(i==TriggerOk)          cut.at(TriggerOk)=1;
     if(i==SignalCandidate)    cut.at(SignalCandidate)=1;
     if(i==TripletKinematics)  cut.at(TripletKinematics)=1;
-    if(i==DeepTauJets)        cut.at(DeepTauJets)=1;
-    if(i==DeepTauMuons)       cut.at(DeepTauMuons)=1;
-    if(i==DeepTauElectrons)   cut.at(DeepTauElectrons)=1;
     if(i==OppositeSide)       cut.at(OppositeSide)=1;
     if(i==OSCharge)           cut.at(OSCharge)=1;
-    if(i==nTaus)              cut.at(nTaus)=1;
+    if(i==nElectrons)         cut.at(nElectrons)=1;
 
   }
 
@@ -53,44 +50,18 @@ void  ZTau3MuTauh::Configure(){
       Nminus1.push_back(HConfig.GetTH1D(Name+c+"_Nminus1_TriggerOk_",htitle,2,-0.5,1.5,hlabel,"Events"));
       Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_TriggerOk_",htitle,2,-0.5,1.5,hlabel,"Events"));
     }
-    else if(i==nTaus){
-      title.at(i)=" At least one $\\tau_{h}$, $pT>20 GeV, |\\eta| < 2.4$ ";
-      hlabel="number of taus";
+    else if(i==nElectrons){
+      title.at(i)=" At least e, $pT>15 GeV, |\\eta| < 2.4$, PF + cutBasedElectronID ( loose WP ) ";
+      hlabel="number of electrons";
       htitle.ReplaceAll("$","");
       htitle.ReplaceAll("\\","#");
-      Nminus1.push_back(HConfig.GetTH1D(Name+c+"_Nminus1_nTaus_",htitle,4,-0.5,3.5,hlabel,"Events"));
-      Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_nTaus_",htitle,4,-0.5,3.5,hlabel,"Events"));
+      Nminus1.push_back(HConfig.GetTH1D(Name+c+"_Nminus1_nElectrons_",htitle,4,-0.5,3.5,hlabel,"Events"));
+      Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_nElectrons_",htitle,4,-0.5,3.5,hlabel,"Events"));
     }
 
-    else if(i==DeepTauJets){
-      title.at(i)=" At least one $\\tau_{h}$ pass DeepTauVsJets (loose WP) ";
-      hlabel=" deep tau jets";
-      htitle.ReplaceAll("$","");
-      htitle.ReplaceAll("\\","#");
-      Nminus1.push_back(HConfig.GetTH1D(Name+c+"_Nminus1_DeepTauJets_",htitle,4,-0.5,3.5,hlabel,"Events"));
-      Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_DeepTauJets_",htitle,4,-0.5,3.5,hlabel,"Events"));
-    }
-
-    else if(i==DeepTauMuons){
-      title.at(i)="$\\tau_{h}$ pass DeepTauVsMuons (loose WP) ";
-      hlabel=" deep tau muons";
-      htitle.ReplaceAll("$","");
-      htitle.ReplaceAll("\\","#");
-      Nminus1.push_back(HConfig.GetTH1D(Name+c+"_Nminus1_DeepTauMuons_",htitle,4,-0.5,3.5,hlabel,"Events"));
-      Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_DeepTauMuons_",htitle,4,-0.5,3.5,hlabel,"Events"));
-    }
-
-    else if(i==DeepTauElectrons){
-      title.at(i)="$\\tau_{h}$ pass DeepTauVsElectrons (loose WP) ";
-      hlabel=" deep tau electrons";
-      htitle.ReplaceAll("$","");
-      htitle.ReplaceAll("\\","#");
-      Nminus1.push_back(HConfig.GetTH1D(Name+c+"_Nminus1_DeepTauElectrons_",htitle,4,-0.5,3.5,hlabel,"Events"));
-      Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_DeepTauElectrons_",htitle,4,-0.5,3.5,hlabel,"Events"));
-    }
 
     else if(i==OppositeSide){
-      title.at(i)="At least one  $\\tau_{h}$ is on opposite side $|\\Delta R| > 1/2$";
+      title.at(i)="At least one e is on opposite side $|\\Delta R| > 1/2$";
       hlabel="delta phi ";
       htitle.ReplaceAll("$","");
       htitle.ReplaceAll("\\","#");
@@ -118,7 +89,7 @@ void  ZTau3MuTauh::Configure(){
       Nminus0.push_back(HConfig.GetTH1D(Name+c+"_Nminus0_SignalCandidate_",htitle,5,-0.5,4.5,hlabel,"Events"));
     }
     else if(i==OSCharge){
-      title.at(i)="Charge $\\tau_{h}$ * $\\tau_{3\\mu}$ =  ";
+      title.at(i)="Charge e * $\\tau_{3\\mu}$ =  ";
       title.at(i)+=cut.at(OSCharge);
       title.at(i)+=" (at least one)";
       htitle=title.at(i);
@@ -134,14 +105,14 @@ void  ZTau3MuTauh::Configure(){
   }
   // Setup NPassed Histogams
 
-  NumberOfTaus=HConfig.GetTH1D(Name+"_NumberOfTaus","NumberOfTaus",5,-0.5,4.5,"Number of #tau ","Events");
 
 
 
-  Tau3MuRelativeIsolation=HConfig.GetTH1D(Name+"_Tau3MuRelativeIsolation","Tau3MuRelativeIsolation",50,0.,1.,"I= p_{T}(#tau)/(p_{T}(#tau) + #sum p_{T})","#Delta R < 0.4 ");
-  TauHDecayMode=HConfig.GetTH1D(Name+"_TauHDecayMode","TauHDecayMode",12,-0.5,11.5,"HPS #tau_{h} decay mode","Events");
-  VisibleDiTauMass=HConfig.GetTH1D(Name+"_VisibleDiTauMass","VisibleDiTauMass",50,20,100,"M_{#tau(h) - #tau(3#mu)}, GeV (visible mass)","Events");
-  MTT=HConfig.GetTH1D(Name+"_MTT","MTT",50,60,120,"M_{#tau(h) - #tau(3#mu)}, GeV (collinear approximation)","Events");
+  Tau3MuRelativeIsolation=HConfig.GetTH1D(Name+"_Tau3MuRelativeIsolation","Tau3MuRelativeIsolation",50,0.,1.1,"I= p_{T}(#tau)/(p_{T}(#tau) + #sum p_{T})","#Delta R < 0.4 ");
+  ElectronSumIsolation=HConfig.GetTH1D(Name+"_ElectronSumIsolation","ElectronSumIsolation",50,0.,10,"I= neutralH + chargedH + photon Iso, GeV","Events ");
+  
+  VisibleDiTauMass=HConfig.GetTH1D(Name+"_VisibleDiTauMass","VisibleDiTauMass",50,20,100,"M_{#tau(#mu) - #tau(3#mu)}, GeV (visible mass)","Events");
+  MTT=HConfig.GetTH1D(Name+"_MTT","MTT",50,60,120,"M_{#tau(#mu) - #tau(3#mu)}, GeV (collinear approximation)","Events");
   TripletMass=HConfig.GetTH1D(Name+"_TripletMass","TripletMass",30,1.4,2.2,"M_{3#mu}, GeV","Events");
 
 
@@ -158,13 +129,13 @@ void  ZTau3MuTauh::Configure(){
 
 
 
-void  ZTau3MuTauh::Store_ExtraDist(){ 
+void  ZTau3MuTaue::Store_ExtraDist(){ 
   ////////////////////////////////////////////////////////////////////////////////////////////////
   // Here you must push back all analysis histograms, otherwise they wont be propagated to the output
 
-  Extradist1d.push_back(&NumberOfTaus);
+
   Extradist1d.push_back(&Tau3MuRelativeIsolation);
-  Extradist1d.push_back(&TauHDecayMode);
+  Extradist1d.push_back(&ElectronSumIsolation);
   Extradist1d.push_back(&VisibleDiTauMass);
   Extradist1d.push_back(&MTT);
   Extradist1d.push_back(&TripletMass);
@@ -176,7 +147,7 @@ void  ZTau3MuTauh::Store_ExtraDist(){
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // This method is called on each event
 
-void  ZTau3MuTauh::doEvent(){ 
+void  ZTau3MuTaue::doEvent(){ 
 
   unsigned int t;
   int id(Ntp->GetMCID());
@@ -232,47 +203,44 @@ void  ZTau3MuTauh::doEvent(){
   pass.at(TripletKinematics) = (value.at(TripletKinematics) == cut.at(TripletKinematics));
 
 
-  std::vector<int> Taus;
-  std::vector<int> Taus_OppositeHemisphere;
-  std::vector<int> Taus_OppositeHemisphere_OppositeCharge;
-  std::vector<int> Taus_DeepTauJetsMedium;
-  std::vector<int> Taus_DeepTauJetsTight;
-  std::vector<int> Taus_DeepTauJetsLoose;
+  std::vector<int> Electrons;
+  std::vector<int> Electrons_OppositeHemisphere;
+  std::vector<int> Electrons_OppositeHemisphere_OppositeCharge;
 
 
 
 
-  for(unsigned int itau=0; itau < Ntp->NTaus(); itau++)
+  for(unsigned int ie=0; ie < Ntp->NElectrons(); ie++)
     {
-      if(Ntp->Tau_P4(itau).Pt() > 20 && fabs(Ntp->Tau_P4(itau).Eta()) < 2.3) Taus.push_back(itau);
+      if(Ntp->Electron_P4(ie).Pt() > 20 && fabs(Ntp->Electron_P4(ie).Eta()) < 2.3) Electrons.push_back(ie);
     }
 
-  value.at(nTaus)  = Taus.size();
-  pass.at(nTaus)  = ( value.at(nTaus) >= cut.at(nTaus) );
+  value.at(nElectrons)  = Electrons.size();
+  pass.at(nElectrons)  = ( value.at(nElectrons) >= cut.at(nElectrons) );
   
 
   value.at(OppositeSide)=0; 
   value.at(OSCharge)    =0;
   if(pass.at(TripletKinematics))
     {
-      for(auto i : Taus)
+      for(auto i : Electrons)
 	{
-	  if(  Ntp->Tau_P4(i).DeltaR(Tau3MuLV) > 0.5   ) Taus_OppositeHemisphere.push_back(i);
+	  if(  Ntp->Electron_P4(i).DeltaR(Tau3MuLV) > 0.5   ) Electrons_OppositeHemisphere.push_back(i);
 	  //	  if(fabs(Ntp->DeltaPhi(Ntp->Tau_P4(i).Phi(), Tau3MuLV.Phi() ))  > TMath::Pi() / 2    ) Taus_OppositeHemisphere.push_back(i);
 
 	}
-      value.at(OppositeSide)= Taus_OppositeHemisphere.size();
+      value.at(OppositeSide)= Electrons_OppositeHemisphere.size();
 
-      for(auto i : Taus_OppositeHemisphere)
+      for(auto i : Electrons_OppositeHemisphere)
 	{
 
 	  int Tau3MuCharge = Ntp->Muon_charge(Ntp->SortedPtMuons(Ntp->ThreeMuonIndices(signal_idx)).at(0)) +
 	    Ntp->Muon_charge(Ntp->SortedPtMuons(Ntp->ThreeMuonIndices(signal_idx)).at(1)) +
 	    Ntp->Muon_charge(Ntp->SortedPtMuons(Ntp->ThreeMuonIndices(signal_idx)).at(2));
 
-	  if(Ntp->Tau_charge(i)*Tau3MuCharge == -1) Taus_OppositeHemisphere_OppositeCharge.push_back(i);
+	  if(Ntp->Electron_charge(i)*Tau3MuCharge == -1) Electrons_OppositeHemisphere_OppositeCharge.push_back(i);
 	}
-      value.at(OSCharge) = Taus_OppositeHemisphere_OppositeCharge.size();
+      value.at(OSCharge) = Electrons_OppositeHemisphere_OppositeCharge.size();
 
     }
 
@@ -282,66 +250,6 @@ void  ZTau3MuTauh::doEvent(){
 
 
 
-  for(auto i : Taus_OppositeHemisphere_OppositeCharge)
-    {
-      if(Ntp->Tau_byLooseDeepTau2017v2p1VSjet(i))  Taus_DeepTauJetsLoose.push_back(i);
-      if(Ntp->Tau_byMediumDeepTau2017v2p1VSjet(i)) Taus_DeepTauJetsMedium.push_back(i);
-      if(Ntp->Tau_byTightDeepTau2017v2p1VSjet(i))  Taus_DeepTauJetsTight.push_back(i);
-      
-      //      Logger(Logger::Info)<<"Tau:  " << i << " Loose/Med/Tight      DM/NewDM   "<<Ntp->Tau_byLooseDeepTau2017v2p1VSjet(i)  << "   "
-      //			  << Ntp->Tau_byMediumDeepTau2017v2p1VSjet(i) <<"  "
-      //			  << Ntp->Tau_byTightDeepTau2017v2p1VSjet(i)  << "           "
-      //			  << Ntp->Tau_DecayModeFinding(i) << "   " 
-	//			  << Ntp-> Tau_NewDecayModeFinding(i)<< "   DM=  "
-      //			  << Ntp->Tau_DecayMode(i) <<std::endl;
-    }
-
-
-  value.at(DeepTauJets) = Taus_DeepTauJetsMedium.size(); // Loose
-  //  value.at(DeepTauJets) = Taus_DeepTauJetsMedium.size();// Medium
-  //  value.at(DeepTauJets) = Taus_DeepTauJetsMedium.size();// Tight
-
-  pass.at(DeepTauJets) = (value.at(DeepTauJets) >= cut.at(DeepTauJets));
-
-
-  value.at(DeepTauMuons) = 0;
-  value.at(DeepTauElectrons) = 0;
-  std::vector<int> PassedDeepMuonsLoose;
-  std::vector<int> PassedDeepMuonsMedium;
-  std::vector<int> PassedDeepMuonsTight;
-
-  for(auto i : Taus_DeepTauJetsMedium)
-    {
-      if(Ntp->Tau_byLooseDeepTau2017v2p1VSmu(i)) PassedDeepMuonsLoose.push_back(i);
-      if(Ntp->Tau_byMediumDeepTau2017v2p1VSmu(i)) PassedDeepMuonsMedium.push_back(i);
-      if(Ntp->Tau_byTightDeepTau2017v2p1VSmu(i)) PassedDeepMuonsTight.push_back(i);
-      
-    }
-    
-  value.at(DeepTauMuons) = PassedDeepMuonsLoose.size();      // Loose
-  //  value.at(DeepTauMuons) = PassedDeepMuonsMedium.size(); // Medium
-  //  value.at(DeepTauMuons) = PassedDeepMuonsTight.size();  // Tight
-
-
-  std::vector<int> PassedDeepElectronsLoose;
-  std::vector<int> PassedDeepElectronsMedium;
-  std::vector<int> PassedDeepElectronsTight;
-  for(auto i : PassedDeepMuonsLoose)
-    {
-      if(Ntp->Tau_byLooseDeepTau2017v2p1VSe(i)) PassedDeepElectronsLoose.push_back(i);
-      if(Ntp->Tau_byMediumDeepTau2017v2p1VSe(i)) PassedDeepElectronsMedium.push_back(i);
-      if(Ntp->Tau_byTightDeepTau2017v2p1VSe(i)) PassedDeepElectronsTight.push_back(i);
-
-    }
-
-
-  value.at(DeepTauElectrons) = PassedDeepElectronsLoose.size();       // Loose
-  //  value.at(DeepTauElectrons) = PassedDeepElectronsMedium.size();  // Medium
-  //  value.at(DeepTauElectrons) = PassedDeepElectronsTight.size();   // Tight
-
-
-  pass.at(DeepTauMuons) = (value.at(DeepTauMuons) >= cut.at(DeepTauMuons));
-  pass.at(DeepTauElectrons) = (value.at(DeepTauElectrons) >= cut.at(DeepTauElectrons));
 
     double wobs=1;
     double w;  
@@ -355,11 +263,9 @@ void  ZTau3MuTauh::doEvent(){
 
   if(status){ 
 
-    unsigned int tau_h_idx = PassedDeepElectronsLoose.at(0);
+    //    std::cout<<"   how many electrons i have  " << Electrons_OppositeHemisphere_OppositeCharge .size() << std::endl;
+    unsigned int electron_idx = Electrons_OppositeHemisphere_OppositeCharge.at(0);
 
-    NumberOfTaus.at(t).Fill(Ntp->NTaus());
-
-    TLorentzVector TauHLV = Ntp->Tau_P4(tau_h_idx);
 
     unsigned int muon_1_idx = Ntp->SortedPtMuons(Ntp->ThreeMuonIndices(signal_idx)).at(0);
     unsigned int muon_2_idx = Ntp->SortedPtMuons(Ntp->ThreeMuonIndices(signal_idx)).at(1);
@@ -375,11 +281,11 @@ void  ZTau3MuTauh::doEvent(){
       Ntp->Muon_charge(Ntp->SortedPtMuons(Ntp->ThreeMuonIndices(signal_idx)).at(2));
 
 
-
+    TLorentzVector ElectronLV = Ntp->Electron_P4(electron_idx);
 
 
     LorentzVectorParticle Tau3MuLVP = Ntp->Tau3mu_LVP(  signal_idx );
-    TVector3 Neutrino_Vect(Ntp->METEt()*TMath::Cos(Ntp->METPhi()),Ntp->METEt()*TMath::Sin(Ntp->METPhi()),Ntp->METEt()/TMath::Tan(TauHLV.Theta()));
+    TVector3 Neutrino_Vect(Ntp->METEt()*TMath::Cos(Ntp->METPhi()),Ntp->METEt()*TMath::Sin(Ntp->METPhi()),Ntp->METEt()/TMath::Tan(ElectronLV.Theta()));
     TLorentzVector Neutrino_LV(Neutrino_Vect,Neutrino_Vect.Mag());
 
 
@@ -388,14 +294,14 @@ void  ZTau3MuTauh::doEvent(){
     float RelativeIsolationMu2 = Ntp->Muon_RelIso(muon_2_idx);
     float RelativeIsolationMu3 = Ntp->Muon_RelIso(muon_3_idx);
 
-    //    std::cout<<"  rel iso  "<<RelativeIsolationMu1<< "   "
-    //	     <<RelativeIsolationMu2 << "    "
-    //	     <<RelativeIsolationMu3 <<std::endl;
+
+
 
     Tau3MuRelativeIsolation.at(t).Fill(    Tau3muLV.Pt()/(RelativeIsolationMu1 + RelativeIsolationMu2 + RelativeIsolationMu3 + Tau3muLV.Pt()),1);
-    TauHDecayMode.at(t).Fill(Ntp->Tau_DecayMode(tau_h_idx), 1);
-    VisibleDiTauMass.at(t).Fill((TauHLV + Tau3muLV).M(), 1);
-    MTT.at(t).Fill( (Tau3muLV + TauHLV  + Neutrino_LV).M(), 1);
+    ElectronSumIsolation.at(t).Fill( Ntp->Electron_puppiPhotonIso(electron_idx)  + Ntp->Electron_puppiChargedHadronIso(electron_idx)  + Ntp->Electron_puppiNeutralHadronIso(electron_idx)   ,1);
+
+    VisibleDiTauMass.at(t).Fill((ElectronLV + Tau3muLV).M(), 1);
+    MTT.at(t).Fill( (Tau3muLV + ElectronLV  + Neutrino_LV).M(), 1);
 
     TripletMass.at(t).Fill(Tau3muLV.M(),1);
 
@@ -403,7 +309,7 @@ void  ZTau3MuTauh::doEvent(){
 }
 
 
-void  ZTau3MuTauh::Finish(){
+void  ZTau3MuTaue::Finish(){
 
   Selection::Finish();
 
---
+++ Code/cherepanov/ZTau3MuTaumu.h
@@ -0,0 +1,57 @@
+#ifndef ZTau3MuTaumu_h
+#define ZTau3MuTaumu_h
+
+#include "Selection.h"
+#include <vector>
+#include "TString.h"
+#include "SimpleFits/FitSoftware/interface/Chi2VertexFitter.h"
+#include "SimpleFits/FitSoftware/interface/TrackParticle.h"
+#include "SimpleFits/FitSoftware/interface/LorentzVectorParticle.h"
+#include "SimpleFits/FitSoftware/interface/ErrorMatrixPropagator.h"
+#include "SimpleFits/FitSoftware/interface/PTObject.h"
+
+#include "SimpleFits/FitSoftware/interface/TPTRObject.h"
+#include "SimpleFits/FitSoftware/interface/GEFObject.h"
+#include "SimpleFits/FitSoftware/interface/GlobalEventFit.h"
+#include "SimpleFits/FitSoftware/interface/TrackHelixVertexFitter.h"
+#include "SimpleFits/FitSoftware/interface/PTObject.h"
+#include "SimpleFits/FitSoftware/interface/TPTRObject.h"
+
+
+class ZTau3MuTaumu : public Selection {
+
+ public:
+  ZTau3MuTaumu(TString Name_, TString id_);
+  virtual ~ZTau3MuTaumu();
+
+  virtual void  Configure();
+  virtual void  Finish();
+
+  enum cuts {TriggerOk=0,
+	     SignalCandidate,
+	     TripletKinematics,
+	     nMuons,
+	     OppositeSide,
+	     OSCharge,
+	     NCuts}; 
+
+
+ protected:
+  virtual void doEvent();  
+  virtual void Store_ExtraDist();
+
+ private:
+
+
+  std::vector<TH1D>   Tau3MuRelativeIsolation;
+  std::vector<TH1D>   OppositeMuRelativeIsolation;
+  std::vector<TH1D>   VisibleDiTauMass;
+  std::vector<TH1D>   MTT;
+  std::vector<TH1D>   TripletMass;
+
+
+
+
+
+};
+#endif
