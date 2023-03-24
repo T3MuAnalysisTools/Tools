93f23fd59ecc41431d91a0966ad356030c8e0d79
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2023-03-24 15:25:57 +0100

    add example of ZTT tmva train

---
+++ Code/CommonUtils/IterativeTrain/TrainConfigs_ZTT.py
@@ -0,0 +1,26 @@
+configuration=[]
+
+
+
+
+# -- 0
+varsets0 = {'ZTT_mu3mu':['tripletpt', 'tripletisolation', 'oppositemuonisolation','ditaumass','muonpt'
+                     ],
+                
+
+            'ZTT_tau3mu':['tripletpt', 'tripletisolation', 'TauPt','ditaumass'
+             ],
+                
+            'ZTT_e3mu':['tripletpt', 'tripletisolation', 'ElectronPt','ditaumass'
+                 
+             ]}
+
+
+
+
+configuration.append(varsets0)
+
+
+
+selection = {'tripletpt': [0,80]
+            }
